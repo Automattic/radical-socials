1e20f39ab460e3713804f412881e575248d032c2
Sarah Norris <sarah@sekai.co.uk>
2026-04-28 10:49:02 +0100

    Reload the page after submitting post

--- plugins/radical-socials/src/frontend/FrontendEditor.js
+++ plugins/radical-socials/src/frontend/FrontendEditor.js
@@ -5,27 +5,17 @@ const SocialEditor = lazy( () => import( './SocialEditor' ) );
 
 export default function FrontendEditor() {
 	const [ expanded, setExpanded ] = useState( false );
-	const [ newPosts, setNewPosts ] = useState( [] );
 
 	function collapse() {
 		setExpanded( false );
 	}
 
-	function handleSuccess( post ) {
-		setNewPosts( ( prev ) => [ post, ...prev ] );
-		collapse();
+	function handleSuccess() {
+		window.location.reload();
 	}
 
 	return (
 		<div className="rs-frontend-editor">
-			{ newPosts.map( ( post ) => (
-				<div
-					key={ post.id }
-					className="rs-new-post"
-					dangerouslySetInnerHTML={ { __html: post.content.rendered } }
-				/>
-			) ) }
-
 			{ ! expanded && (
 				<button className="rs-editor-prompt" onClick={ () => setExpanded( true ) }>
 					What&apos;s on your mind?
