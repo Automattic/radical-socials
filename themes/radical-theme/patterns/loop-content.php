91c22bd8adbc4275d7b0fc34f7728ab583c544d8
MaggieCabrera <maggie.cabrera@automattic.com>
2026-05-14 12:28:06 +0200

    fix template structure

---
+++ themes/radical-theme/patterns/loop-content.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Title: Loop content
+ * Slug: radical-theme/loop-content
+ * Categories: 
+ * Inserter: no
+ */ 
+?>
+<!-- wp:post-template {"metadata":{"ignoredHookedBlocks":["radical-socials/like-button"]}} -->
+
+	<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+		
+	<!-- wp:post-featured-image {"isLink":true,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} /-->
+	<!-- wp:post-title {"isLink":true,"style":{"spacing":{"margin":{"bottom":"0"}}}} /-->
+	<!-- wp:post-date {"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}},"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"small"} /-->
+
+	<!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+	<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+	
+	<!-- wp:post-content {"metadata":{"ignoredHookedBlocks":["activitypub/reactions"]}} /-->
+
+	<!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+	<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+
+	<!-- wp:read-more {"content":"Read more"} /-->
+
+	<!-- wp:group {"style":{"spacing":{"blockGap":"0.5em"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+	<div class="wp-block-group"><!-- wp:post-terms {"term":"rs_feed_type","prefix":"Origin: ","className":"rs-feed-type-badge"} /-->
+
+	<!-- wp:post-terms {"term":"rs_source","prefix":"Author: ","className":"rs-feed-source-label"} /--></div>
+	<!-- /wp:group -->
+
+	<!-- wp:activitypub/reactions {"className":"is-style-facepile"} -->
+	<div class="wp-block-activitypub-reactions is-style-facepile"><!-- wp:heading {"level":6} -->
+	<h6 class="wp-block-heading">Fediverse reactions</h6>
+	<!-- /wp:heading --></div>
+	<!-- /wp:activitypub/reactions -->
+
+	<!-- wp:group {"style":{"spacing":{"blockGap":"0.5em"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group"><!-- wp:post-comments-link /--></div>
+	<!-- /wp:group -->
+
+	<!-- wp:radical-socials/like-button /--></div>
+	<!-- /wp:group -->
+
+	<!-- wp:separator {"className":"is-style-default","style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}}} -->
+	<hr class="wp-block-separator has-alpha-channel-opacity is-style-default" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)"/>
+	<!-- /wp:separator -->
+<!-- /wp:post-template -->
+
+<!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"space-between"}} -->
+<!-- wp:query-pagination-previous /-->
+
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination -->
