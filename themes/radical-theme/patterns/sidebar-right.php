9b98cedcd357fd2b7958712c2a8d08f500b97b7e
MaggieCabrera <maggie.cabrera@automattic.com>
2026-05-20 19:15:02 +0200

    tweak design more

--- themes/radical-theme/patterns/sidebar-right.php
+++ themes/radical-theme/patterns/sidebar-right.php
@@ -17,7 +17,7 @@
 		<h2 class="wp-block-heading has-medium-font-size"><?php echo esc_html__( 'Filter by source', 'radical-theme' ); ?></h2>
 		<!-- /wp:heading -->
 
-		<!-- wp:categories {"taxonomy":"rs_feed_type","style":{"typography":{"lineHeight":"2"},"spacing":{"padding":{"right":"0","left":"var:preset|spacing|30","top":"0","bottom":"0"}}}} /-->
+		<!-- wp:categories {"taxonomy":"rs_feed_type","style":{"typography":{"lineHeight":"2.5"},"spacing":{"padding":{"right":"0","left":"var:preset|spacing|30","top":"0","bottom":"0"}}}} /-->
 	</div>
 	<!-- /wp:group -->
 
