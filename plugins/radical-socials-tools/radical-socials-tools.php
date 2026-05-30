ca07b033b57a2ae1ef6735c011fafda6c3d7ecec
MaggieCabrera <maggie.cabrera@automattic.com>
2026-05-30 19:50:58 +0200

    Bump plugin version to 1.0.1

--- plugins/radical-socials-tools/radical-socials-tools.php
+++ plugins/radical-socials-tools/radical-socials-tools.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Radical Socials Tools
  * Plugin URI:        https://github.com/Automattic/radical-socials
  * Description:       Escape walled gardens with a self-hosted WordPress site that feels like home.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 6.7
  * Tested up to:      7.0
  * Requires PHP:      8.0
