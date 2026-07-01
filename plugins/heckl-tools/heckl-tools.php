321811b6960d56d89757130006bf5e16c7b27f1a
MaggieCabrera <maggie.cabrera@automattic.com>
2026-07-01 13:42:28 +0200

    Bump plugin version to 1.0.4

--- plugins/heckl-tools/heckl-tools.php
+++ plugins/heckl-tools/heckl-tools.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Heckl Tools
  * Description:       Escape walled gardens with a self-hosted WordPress site that feels like home.
- * Version:           1.0.3
+ * Version:           1.0.4
  * Requires at least: 6.7
  * Tested up to:      7.0
  * Requires PHP:      8.0
